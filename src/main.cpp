f1ab80ff175a51a8f32795fc052d26a68368f137
kisozinov <71232712+kisozinov@users.noreply.github.com>
2020-11-09 22:32:39 +0300

    peretest

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main()
 		cout << "Number " << num1.getValue() << " is really prime!" << endl;
 	else
 		cout << "Number " << num1.getValue() << " not is prime!" << endl;
-	//////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
 	cout << "Enter one more number" << endl;
 	cin >> n;
 	num2.setValue(n);
@@ -24,7 +24,7 @@ int main()
 		cout << "Number " << num2.getValue() << " is really prime!" << endl;
 	else
 		cout << "Number " << num2.getValue() << " not is prime!" << endl;
-	//////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////////
 	cout << "Amount of prime numbers between "<< num1.getValue() << " and " <<  num2.getValue() <<" is " << num1.countBetween(num2) << endl;
 	cout << "Next prime number after "<< num1.getValue() << " is " << num1.nextPrime().getValue() << endl;
 	return 0;
