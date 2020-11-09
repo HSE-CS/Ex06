03a01d536dafcda7c36498a2b51f33f3a7bdaea3
GlebNeshchetkin <glebneshchetkin@gmail.com>
2020-11-09 19:11:06 +0300

    Test

---
+++ include/Prime.h
@@ -0,0 +1,19 @@
+#ifndef _PRIME_H
+#define _PRIME_H_
+#include<cmath>
+//typedef unsigned int Prime;
+#include <iostream>
+using namespace std;
+class Prime
+{
+private:
+    unsigned int prime;
+public:
+    void setValue(int);
+    bool isValid();
+    bool testPrime(int);
+    int countBetween(Prime&);
+    Prime nextPrime();
+    int getValue();
+};
+#endif
