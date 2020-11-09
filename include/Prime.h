a400171effbd256004140cc0a89c8934abe28075
valvarl <valvarl@ya.ru>
2020-11-09 19:17:29 +0300

    task1

---
+++ include/Prime.h
@@ -0,0 +1,16 @@
+//
+// Created by freeb on 09.11.2020.
+//
+
+#ifndef TASK_PRIME_H
+#define TASK_PRIME_H
+
+
+
+class Prime {
+
+};
+
+
+
+#endif //TASK_PRIME_H
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by freeb on 09.11.2020.
+//
+
+#include "main.h"
