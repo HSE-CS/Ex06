335e77adfffd595f916bc0952fe1fbc76a5d359e
DenisKabanov <dskabanov@edu.hse.ru>
2020-11-09 21:28:31 +0300

    Denis_Kabanov_b71

---
+++ src/Prime.cpp
@@ -0,0 +1,61 @@
+#include "Prime.h"
+
+void Prime::setValue(int value)
+{
+	this->prime = value;
+}
+
+bool Prime::checkPrime(int value)
+{
+	int flag = 1;
+	if (value == 1)
+		flag=0;
+	else
+	{
+		for (int i = 2; i <= sqrt(value); i++)
+		{
+			if (value % i == 0)
+				flag = 0;
+		}
+	}
+	if (flag == 1)
+		return true;
+	else 
+		return false;
+}
+
+bool Prime::isValid()
+{
+	return checkPrime(this->prime);
+}
+
+Prime Prime::nextPrime()
+{
+	int nextprime = this->prime + 1;
+	while (checkPrime(nextprime) != true)
+		nextprime++;
+	Prime primenext;
+	primenext.setValue(nextprime);
+	return primenext;
+}
+
+int Prime::countBetween(Prime& value)
+{
+	int left = min(this->prime,value.prime);
+	int right = max(this->prime, value.prime);
+	//cout <<endl<< left <<" "<< right << endl;
+	int counter=0;
+	while (left < right)
+	{
+		left++;
+		if ((checkPrime(left) == true) && (left != right))
+			//cout << left<<endl;
+			counter++;
+	}
+	return counter;
+}
+
+int Prime::getValue()
+{
+	return this->prime;
+}
