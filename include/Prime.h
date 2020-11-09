614fc7b714173f249f7048755a559a49e539efa1
Irina <pasmanikirina29@gmail.com>
2020-11-09 17:29:33 +0300

    try

---
+++ include/Prime.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef  PRIME_H
+#define PRIME_H
+
+class Prime {
+private:
+	int value;
+	bool isPrime;
+public:
+	void setValue(int); //установить число
+	bool isValid();  //проверка, действительно ли установленное число простое
+	bool testPrime(int);  //проверка переданного числа на простоту
+	int countBetween(Prime&);  //подсчет количества простых чисел между установленным и переданным
+	Prime nextPrime();  //возвращение простого числа, расположенного следом за установленным
+	int getValue();  //возвращение установленного числа  
+};
+
+
+#endif // ! PRIME_H
