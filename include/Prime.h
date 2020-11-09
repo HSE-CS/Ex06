7e4a8eec237f92e4b7bba9019b11bbd85c774278
NikDemoShow <Nixon2001@yandex.ru>
2020-11-09 20:26:16 +0300

    first try

---
+++ include/Prime.h
@@ -0,0 +1,12 @@
+
+class Prime {
+private:
+	int num;
+public:
+	void setValue(int x);
+	bool isValid();
+	bool checkPrime(int x);
+	int countBetween(Prime&);
+	Prime nextPrime();
+	int getValue();
+};
