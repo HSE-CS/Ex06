e6cbe6e7916a9421b006e326b4c64ae9204310c1
valvarl <valvarl@ya.ru>
2020-11-09 22:51:05 +0300

    lost commit

--- include/Prime.h
+++ include/Prime.h
@@ -6,11 +6,17 @@
 #define TASK_PRIME_H
 
 
-
 class Prime {
-
+private:
+    int value;
+public:
+    void setValue(int x);
+    bool isValid();
+    bool checkPrime(int x);
+    int countBetween(Prime& x);
+    Prime nextPrime();
+    int getValue();
 };
-
 
 
 #endif //TASK_PRIME_H
