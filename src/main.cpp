d193a7ac6146209e779d4ab9233bb626e95cdf2f
Right-bugor <innokentiyyyyy@gmail.com>
2020-11-09 21:34:42 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,17 @@ int main(){
 
 	bool ans = num2.isValid();        // true
 	cout << "ans=(" << ans << ")\n";
+	////////////////////////////////
+	num1.setValue(193);
+	num2 = num1.nextPrime();
+	cout << "193->=(" << num2.val << ")\n";//197
+
+	num1.setValue(3);
+	num2.setValue(13);
+	cout << "{3,13}->=(" << num1.countBetween(num2) << ")\n";//3
+
+	cout << "18-prime?->=(" << num1.checkPrime(18) << ")\n";//no
+	cout << "149-prime?->=(" << num1.checkPrime(149) << ")\n";//yes
 
 	return 0;
 }
