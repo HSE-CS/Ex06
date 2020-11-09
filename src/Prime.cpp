47964e9bec8a8227e6d15e72bd6706b4c05fc126
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2020-11-09 18:32:14 +0300

    Add files via upload

---
+++ src/Prime.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Алексей Трутнев on 09.11.2020.
+//
+
+#include "Prime.h"
+#include <iostream>
+
+void Prime::setValue(int n){
+    value = n;
+    prime = checkPrime(n);
+}
+
+bool Prime::isValid(){
+    return prime;
+}
+
+bool Prime::checkPrime(int a){
+    if (a < 2)
+        return false;
+    for (int i = 2; true; ++i){
+        int q = a / i;
+        if (q < i)
+            return true;
+        if (a == q * i)
+            return false;
+    }
+}
+
+int Prime::countBetween(Prime& temp){
+    size_t count = 0;
+    for (size_t i = std::min(value, temp.value); i < std::max(value, temp.value); ++i)
+        if(checkPrime(i)) count++;
+    return count;
+}
+
+Prime Prime::nextPrime(){
+    size_t n_prime = value + 1;
+    while(!checkPrime(n_prime))
+        n_prime++;
+    Prime temp{};
+    temp.setValue(n_prime);
+    return temp;
+}
+
+int Prime::getValue(){
+    return value;
+}
