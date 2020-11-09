e09f0055ae528fe80a747b1f4a642c2de86e00e5
Ilya Urtyokov <urtilya@gmail.com>
2020-11-09 15:59:42 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include "Prime.h"
 #include <iostream>
 
+using namespace std;
+
 
 int main()
 {
     Prime num1,num2;
     num1.setValue(2);
-    std::cout<<num1.isValid()<<endl;
-    std::cout<<num1.getValue()<<endl;
+    cout<<num1.isValid()<<endl;
+    cout<<num1.getValue()<<endl;
     num2=num1.nextPrime();  
-    std::cout<<num2.getValue()<<endl;       
+    cout<<num2.getValue()<<endl;       
     bool result=num1.testPrime(17); 
-    std::cout<<result<<endl;
+    cout<<result<<endl;
     num2.setValue(17);              
     int count=num2.countBetween(num1);
-    std::cout<<count<<endl; 
+    cout<<count<<endl; 
            
    return 0;
 }
