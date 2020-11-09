4e72ba3a9b9f5a59c62ec691150a726cdf09aea3
Leonidek23 <rad.shabarshin@mail.ru>
2020-11-10 00:33:25 +0300

    ShabarshinL

--- src/Prime.cpp
+++ src/Prime.cpp
@@ -6,16 +6,35 @@ void Prime::setValue(int n) {
     prime = checkPrime(n);
 }
 bool Prime::isValid() {
-    return prime;
+    return checkPrime(this->value);
 }
 bool Prime::checkPrime(int a) {
-
+    if (val == 1)
+        return false;
+    for (auto i = 2; i <= sqrt(val); i++)
+    {
+        if (val % i == 0)
+            return false;
+    }
+    return true;
 }
 int Prime::countBetween(Prime& temp) {
-
+    int min_val = std::min(prime.value, this->value);
+    int max_val = std::max(prime.value, this->value);
+    Prime curr_prime;
+    curr_prime.setValue(min_val);
+    curr_prime.setNextPrime();
+    int count = 0;
+    while (curr_prime.value < max_val)
+    {
+        count++;
+        curr_prime.setNextPrime();
+    }
+    return count;
 }
 
 Prime Prime::nextPrime() {
+    this->setValue(this->nextPrime().getValue());
 }
 
 int Prime::getValue() {
