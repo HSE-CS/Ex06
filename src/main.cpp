17b3c72f97b673ee9f16b7475a61fca7ba8228da
Denis Golovanov <dmgolovanov@edu.hse.ru>
2020-11-09 23:46:31 +0300

    Add task

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Prime.h"
+using namespace std;
+
+int main() {
+	Prime num1, num2;
+	num1.setValue(13);
+	num2 = num1.nextPrime();
+	cout << num2.getValue() << endl;
+	bool result = num1.checkPrime(17); 
+	num2.setValue(3);              
+	int count = num2.countBetween(num1);
+	cout << count << endl;
+	bool ans = num2.isValid();        
+
+	return 0;
+}
