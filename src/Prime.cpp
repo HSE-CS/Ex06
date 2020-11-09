77d8dce8cd5db1a04cfe3baefd66d3abe4cdd53f
Leonidek23 <rad.shabarshin@mail.ru>
2020-11-09 23:45:31 +0300

    ShabarshinL

--- src/Prime.cpp
+++ src/Prime.cpp
@@ -5,3 +5,19 @@ void Prime::setValue(int n) {
     value = n;
     prime = checkPrime(n);
 }
+bool Prime::isValid() {
+    return prime;
+}
+bool Prime::checkPrime(int a) {
+
+}
+int Prime::countBetween(Prime& temp) {
+
+}
+
+Prime Prime::nextPrime() {
+}
+
+int Prime::getValue() {
+    return value;
+}
