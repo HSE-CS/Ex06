f700dc0cc7cb2c09ec99d368e90079057dec70f5
Vlad <zagoskin.3@mail.ru>
2020-11-09 18:54:04 +0300

    1

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,23 @@
 int main()
 {
 	Prime num;
-	num.setValue(2);
-	int result=num.getValue();
-	std :: cout << result << std :: endl;
-	num.setValue(21);
-	bool result1=num.isValid();
-	std :: cout << result1 << std :: endl;
-	Prime num1,num2;
-	num1.setValue(193);
-	num2=num1.nextPrime();
-	int result2=num2.getValue();
-	std :: cout << result2 << std :: endl;
-	num1.setValue(3);
-	num2.setValue(13);
-	int result3=num1.countBetween(num2);
-	std :: cout << result3 << std :: endl;
-	bool result4=num.checkPrime(18);
-	std :: cout << result4 << std :: endl;
+	nu.setValue(2);
+	int resul=nu.getValue();
+	std :: cout << resul << std :: endl;
+	nu.setValue(21);
+	bool resul1=nu.isValid();
+	std :: cout << resul1 << std :: endl;
+	Prime nu1,nu2;
+	nu1.setValue(193);
+	nu2=nu1.nextPrime();
+	int resul2=nu2.getValue();
+	std :: cout << resul2 << std :: endl;
+	nu1.setValue(3);
+	nu2.setValue(13);
+	int resul3=nu1.countBetween(num2);
+	std :: cout << resul3 << std :: endl;
+	bool resul4=nu.checkPrime(18);
+	std :: cout << resul4 << std :: endl;
 	
 	
 }
