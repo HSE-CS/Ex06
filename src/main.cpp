ae6c13952189d57bca3609edceb8b14e0a9be35c
Vlad <zagoskin.3@mail.ru>
2020-11-09 22:07:59 +0300

    1

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "Prime.h"
+#include <cstring>
+#include <iostream>
+
+
+int main()
+{
+	Prime nu;
+	nu.setValue(2);
+	int resul=nu.getValue();
+	std :: cout << resul << std :: endl;
+	nu.setValue(21);
+	bool resul1=nu.isValid();
+	std :: cout << resul1 << std :: endl;
+	Prime nu1,nu2;
+	nu1.setValue(193);
+	nu2=nu1.nextPrime();
+	int resul2=nu2.getValue();
+	std :: cout << resul2 << std :: endl;
+	nu1.setValue(3);
+	nu2.setValue(13);
+	int resul3=nu1.countBetween(num2);
+	std :: cout << resul3 << std :: endl;
+	bool resul4=nu.checkPrime(18);
+	std :: cout << resul4 << std :: endl;
+	
+	
+}
