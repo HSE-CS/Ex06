cc4be088d05fd6bace55a8c48827d04167ed11fc
Nikita Kasyanov <kasyanikita@gmail.com>
2020-11-09 15:32:53 +0300

    First attempt

---
+++ src/Prime.cpp
@@ -0,0 +1,49 @@
+#include "../include/Prime.h"
+
+void Prime::setValue(int num) {
+    this->num = num;
+}
+
+int Prime::getValue() {
+    return num;
+}
+
+bool Prime::isValid() {
+    for(int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Prime::testPrime(int num) {
+    for(int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Prime Prime::nextPrime() {
+    int val = num + 1;
+    while(!testPrime(val)) {
+        ++val;
+    }
+    Prime next_prime;
+    next_prime.num = val;
+    return next_prime;
+}
+
+int Prime::countBetween(Prime& prime) {
+    int begin = this->num + 1;
+    int end = prime.num - 1;
+    int count = 0;
+    for (int i = begin; i <= end; ++i) {
+        if (testPrime(i)) {
+            ++count;
+        }
+    }
+    return count;
+}
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "../include/Prime.h"
+#include <iostream>
+
+int main() {
+  Prime num1, num2;
+  num1.setValue(4);
+  std::cout << num1.getValue() << " - ";
+  if (num1.isValid()) {
+    std::cout << "prime number" << '\n';
+  } else {
+    std::cout << "not prime number" << '\n';
+  }
+  num1.setValue(5);
+  num2.setValue(23);
+  std::cout << "Next prime number after " << num1.getValue()
+            << " is " << num1.nextPrime().getValue() << '\n';
+  int test_number = 17;
+  if (num1.testPrime(17)) {
+    std::cout << test_number << " - prime number" << '\n';
+  } else {
+    std::cout << test_number << " - not prime number" << '\n';
+  }
+  std::cout << "There are " << num1.countBetween(num2) << " prime numbers between "
+            << num1.getValue() << " and " << num2.getValue() << '\n';
+}
