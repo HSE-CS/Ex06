bea531e4a0bbb7a8cdf8a465968de6f1dbd94ca5
__dolt_coder__ <ihackit595@gmail.com>
2020-11-09 20:21:31 +0300

    first commit

---
+++ src/Prime.cpp
@@ -0,0 +1,40 @@
+#include "../include/Prime.h"
+
+void Prime::setValue(int v) {
+    val = v;
+}
+
+bool Prime::isValid() {
+    return checkPrime(val);
+}
+
+bool Prime::checkPrime(int v) {
+    int dels = 0;
+    for(int i = 2; i < v; ++i) {
+        if (v % i == 0)
+            dels++;
+    }
+    return (dels) ? false : true;
+}
+
+int Prime::countBetween(Prime& pNum) {
+    int c = 0;
+    for (int i = val + 1; i < pNum.getValue(); ++i) {
+        if(checkPrime(i))
+            c++;
+    }
+    return c;
+}
+
+Prime Prime::nextPrime() {
+    Prime newPrime;
+    newPrime.setValue(val + 1);
+    while(!checkPrime(newPrime.getValue())) 
+        newPrime.setValue(newPrime.getValue() + 1);
+
+    return newPrime;
+}
+
+int Prime::getValue() {
+    return val;
+}
---
+++ src/main1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../include/Prime.h"
+
+int main () {
+    Prime pNum;
+    pNum.setValue(5);
+    std::cout << pNum.getValue() <<"\n";
+    std::cout << pNum.isValid() <<"\n";
+    std::cout << pNum.checkPrime(8) <<"\n";
+    
+    Prime pNext = pNum.nextPrime();
+    Prime pNextTwo = pNext.nextPrime();
+    Prime pNextThree = pNextTwo.nextPrime();
+
+    std::cout << pNext.getValue() <<"\n";
+    std::cout << pNextTwo.getValue() <<"\n";
+
+    std::cout << pNum.countBetween(pNextThree) <<"\n";
+
+    
+    return 0;
+}
