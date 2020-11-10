312e193813aeae3a794fb2c5a2f364e9692336cb
unknown <dakozlova_1@edu.hse.ru>
2020-11-10 22:33:55 +0300

    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,10 @@ cout << v.getValue()<< endl;
 Prime w;
 w = v.nextPrime();
 
-bool tf1 = v.isValid;
-cout << tf1 << endl;
+bool tf = v.isValid;
+cout << tf << endl;
 
-bool tf2 = v.checkPrime(13);
+bool tf1 = v.checkPrime(13);
 cout << tf1 << endl;
 
 int count = w.countBetween(v);
