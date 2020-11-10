a8d773afa971234ef4cabf75b99d8bd77bb68c7b
Stolbov Yaroslav <stojar2001@gmail.com>
2020-11-10 23:24:57 +0300

    firs try

---
+++ include/Prime.h
@@ -0,0 +1,22 @@
+//
+// Created by stoja on 10.11.2020.
+//
+
+#ifndef TASK_PRIME_H
+#define TASK_PRIME_H
+
+class Prime{
+private:
+    int value;
+public:
+    void setValue(int);
+    bool isValid();
+    bool checkPrime(int);
+    int countBetween(Prime&);
+    Prime nextPrime();
+    int getValue();
+
+};
+
+
+#endif //TASK_PRIME_H
