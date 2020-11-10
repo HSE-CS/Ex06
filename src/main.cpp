74396066fcd6a110d01f98d04d0757237978db0f
Andrey <bakurskii2001@gmail.com>
2020-11-10 13:20:13 +0300

    add task

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "Prime.h"
+
+using namespace std;
+
+int main() {
+
+	Prime num1, num2;
+
+    num1.setValue(2);
+    cout << num1.getValue() << endl;
+
+    num2 = num1.nextPrime(); // в num2 устанавливается 3
+    cout << num2.getValue() << endl;
+
+    bool result = num1.checkPrime(17); // true
+    cout << result << endl;
+
+    num2.setValue(17);     // в num2 устанавливаем 17
+
+    int count = num2.countBetween(num1); // count=5
+    
+    bool ans = num2.isValid();        // true
+    cout << ans << endl;
+
+	return 0;
+}
