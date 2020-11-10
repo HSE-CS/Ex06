a1ea7ce14c43f0b9ae40c51709536c345104abbe
Polina Ozhiganova <44429468+DoktaPola@users.noreply.github.com>
2020-11-10 12:08:49 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Prime.h"
+
+int main(){  
+    Prime num1,num2;
+
+    num1.setValue(3);
+
+    num2 = num1.nextPrime();
+    std::cout << "Next prime after 3 is " << num2.getValue() << std::endl;
+
+    std::cout << "Is 15 prime? " << num1.checkPrime(15) << std::endl;
+
+    num2.setValue(83);
+    std::cout << "Is 83 valid? " << num2.isValid() << std::endl;;
+
+    std::cout << "Amount of primes between 3 and 83: " << num2.countBetween(num1) << std::endl;
+}
