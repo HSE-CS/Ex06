920d883099ddb0a2dfa5737e01278e4d62a61fb0
IrinaZarubina <63425301+IrinaZarubina@users.noreply.github.com>
2020-11-10 16:39:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include "Prime.h"
-using namespace std;
+#include <iostream>
+#include <cstring>
+
 int main()
 {
-	return 0;
+Prime num1, num2;
+
+    num1.setValue(4);
+    std::cout << num1.isValid() << std::endl;
+
+    num2 = num1.nextPrime();          
+    std::cout << num2.getValue() << std::endl;
+
+    bool result = num1.checkPrime(17); 
+    std::cout << result << std::endl;
+
+    num2.setValue(17);              
+
+    int count = num2.countBetween(num1); // count=5
+    std::cout << count << std::endl;
+return 0;
 }
