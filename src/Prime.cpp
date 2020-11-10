7036d3989f0d62e9bf8c91e03951b9e596118001
valvarl <valvarl@ya.ru>
2020-11-10 19:55:23 +0300

    testi suka uzass...

--- src/Prime.cpp
+++ src/Prime.cpp
@@ -24,12 +24,12 @@ bool Prime::checkPrime(int x) {
 int Prime::countBetween(Prime &x) {
     int c = 0;
     if (value < x.value){
-        for (int i = value; i < x.value; ++i) {
+        for (int i = value+1; i < x.value; ++i) {
             if (Prime::checkPrime(i)) { c++; }
         }
     }
     else if (x.value < value) {
-        for (int i = x.value; i < value; ++i) {
+        for (int i = x.value+1; i < value; ++i) {
             if (Prime::checkPrime(i)) { c++; }
         }
     }
