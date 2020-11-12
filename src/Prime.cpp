0ff6af9275a25c909268bd6c192907ea30c7766f
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-11-12 13:34:26 +0300

    fixed bug in nextPrime

--- src/Prime.cpp
+++ src/Prime.cpp
@@ -18,10 +18,10 @@ bool Prime::checkPrime(int num)
 int Prime::countBetween(Prime& second)
 {
   int cnt = 0;
-  for (int i = second.value + 1; i < value; i++)
+  for (int i = value + 1; i < second.value; i++)
     if (checkPrime(i))
       cnt++;
-      
+
   return cnt;
 }
 
@@ -29,7 +29,7 @@ Prime Prime::nextPrime()
 {
   int copy_value = value;
   copy_value++;
-  while (!checkPrime(value))
+  while (!checkPrime(copy_value))
     copy_value++;
 
   Prime ans;
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main()
 {
     Prime num1,num2;
-    num1.setValue(2);
+    num1.setValue(13);
     num2=num1.nextPrime();
+    cout << num2.getValue() << endl;
     bool result = num1.checkPrime(17);
-    num2.setValue(17);
+    num2.setValue(3);
     int count = num2.countBetween(num1);
+    cout << count << endl;
     bool ans=num2.isValid();
 }
