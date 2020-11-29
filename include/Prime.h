53d77d06ec5d718b7704741a64843d4c015c9a10
Nikita <semaev2014@yandex.ru>
2020-11-29 18:32:33 +0300

    Checking travis performance when specifying relative library paths

---
+++ include/Prime.h
@@ -0,0 +1,8 @@
+#ifndef PRIME_H_
+#define PRIME_H_
+
+
+
+
+
+#endif /* PRIME_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../include/Prime.h"
+using namespace std;
+
+int main(){
+	cout << "Hello!";
+	return 0;
+}
