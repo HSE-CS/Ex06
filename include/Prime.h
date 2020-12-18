47285bb8845ce174bd7a77d50472d7c13386e590
Julia Rogozyan <juliarogozyan@Air-Julia.mts.local>
2020-12-18 17:28:20 +0300

    Rogozyan

---
+++ include/Prime.h
@@ -0,0 +1,11 @@
+class Prime {
+    private:
+        int val;
+    public:
+        void setValue(int v);
+        bool isValid();
+        bool checkPrime(int v);
+        int countBetween(Prime& pNum);
+        Prime nextPrime();
+        int getValue();
+};
